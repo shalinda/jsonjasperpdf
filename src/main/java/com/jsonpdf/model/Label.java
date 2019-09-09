f641ebbfabcb4d1dda6c3a8be43075012f02cc7b
shalinda <shalinda@gmail.com>
2019-09-08 21:34:55 -0400

    label report

---
+++ src/main/java/com/jsonpdf/model/Label.java
@@ -0,0 +1,42 @@
+package com.jsonpdf.model;
+
+public class Label {
+
+    private String style;
+    private String customerLeather;
+    private String color;
+    private String size;
+
+    public String getStyle() {
+        return style;
+    }
+
+    public String getCustomerLeather() {
+        return customerLeather;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public void setCustomerLeather(String customerLeather) {
+        this.customerLeather = customerLeather;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+}
